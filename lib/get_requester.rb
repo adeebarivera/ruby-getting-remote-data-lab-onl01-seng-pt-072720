93e9f5479e117226c51df5d8da7ee7ee2b6af0a0
adeeba reaz <adeebarivera@outlook.com>
2020-09-07 04:11:28 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,20 +3,23 @@ require 'open-uri'
 require 'json'
 
 class GetRequester
- 
-  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
-  
- 
-  def get_response_body
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-  
+    attr_accessor :url
 
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        items = JSON.parse(self.get_response_body)
+        items.collect do | item |
+            item["individual"]
+        end
+        items
+    end
 end
- 
-parse_jason = GetRequester.new.get_response_body
-puts parse_jason
-
-
