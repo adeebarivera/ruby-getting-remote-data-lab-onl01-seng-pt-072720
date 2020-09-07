330529d3fe5f48521f9024ad5772aba99e9308e0
adeeba reaz <adeebarivera@outlook.com>
2020-09-07 03:25:00 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,7 +6,7 @@ class GetRequester
  
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
  
-  def get_programs
+  def get_response_body
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
@@ -14,6 +14,6 @@ class GetRequester
  
 end
  
-programs = GetPrograms.new.get_programs
+programs = GetPrograms.new.
 puts programs
  
